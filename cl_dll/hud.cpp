189fb4d5df4fc18db46cb2643758a4932d373a86
FreeSlave <freeslave93@gmail.com>
2025-07-17 21:26:59 +0300

    Save default_fov cvar to the config

--- cl_dll/hud.cpp
+++ cl_dll/hud.cpp
@@ -403,7 +403,7 @@ void CHud :: Init( void )
 	m_iHUDColor2 = 0x004B00A8; //75, 0, 168 -- LRC
 
 	CVAR_CREATE( "zoom_sensitivity_ratio", "1.2", 0 );
-	default_fov = CVAR_CREATE( "default_fov", "90", 0 );
+	default_fov = CVAR_CREATE( "default_fov", "90", FCVAR_ARCHIVE );
 	m_pCvarStealMouse = CVAR_CREATE( "hud_capturemouse", "1", FCVAR_ARCHIVE );
 	m_pCvarDraw = CVAR_CREATE( "hud_draw", "1", FCVAR_ARCHIVE );
 	cl_lw = gEngfuncs.pfnGetCvarPointer( "cl_lw" );
